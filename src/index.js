203dda4857b98c15d89c39255933fbaf51dba3e2
Joe Pea <joe@trusktr.io>
2018-01-05 14:25:00 -0800

    forgot the passThrough arg

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 /**
  * Await for the DOM's sub-resources to have been loaded (images, scripts,
  * etc).  This corresponds to the `window`'s "load" event.
- *
- * @example
- * ```js
- * async function main() {
- *     await windowLoaded()
- *     console.log('Window loaded!')
- * }
- * main()
- * ```
  */
 export default
-async function windowLoaded() {
+async function windowLoaded( _passThrough ) {
+
     if (document.readyState !== 'complete') {
         await new Promise(function(resolve) {
             window.addEventListener('load', resolve)
         })
     }
+
+    return _passThrough
 }
